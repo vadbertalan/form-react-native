bf5a6d4214bbc6cfa4bd8c83a929f679eabc7a5e
Vad Bertalan <vad.bertalan@codespring.ro>
2024-01-16 19:25:31 +0200

    ui: update account type picker's style

--- src/components/account-type-picker.tsx
+++ src/components/account-type-picker.tsx
@@ -1,6 +1,8 @@
 import {Picker} from '@react-native-picker/picker';
 import React, {FC} from 'react';
 import {LoginAccountType} from '../models/login-payload';
+import {StyleSheet, View} from 'react-native';
+import {COLORS, METRICS} from '../style/common-styles';
 
 type AccountTypePickerProps = {
   selectedType: LoginAccountType;
@@ -12,14 +14,25 @@ export const AccountTypePicker: FC<AccountTypePickerProps> = ({
   selectedType,
 }) => {
   return (
-    <Picker selectedValue={selectedType} onValueChange={onNewTypeSelected}>
-      {Object.values(LoginAccountType).map(accountType => (
-        <Picker.Item
-          label={accountType}
-          value={accountType}
-          key={accountType}
-        />
-      ))}
-    </Picker>
+    <View style={styles.container}>
+      <Picker selectedValue={selectedType} onValueChange={onNewTypeSelected}>
+        {Object.values(LoginAccountType).map(accountType => (
+          <Picker.Item
+            label={accountType}
+            value={accountType}
+            key={accountType}
+          />
+        ))}
+      </Picker>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: COLORS.line,
+    borderBottomLeftRadius: METRICS.baseBorderRadius,
+    margin: METRICS.baseMargin,
+  },
+});
